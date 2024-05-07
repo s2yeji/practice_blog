487ebedbc0d4e9f34b9bc0bf86ad059736fa7e21
s2yeji <s2seolyeji@gmail.com>
2024-05-07 17:33:58 +0900

    2024-05-07 edit

--- server copy.js
+++ server copy.js
@@ -1,39 +1,50 @@
 require('dotenv').config()
 
-const express = require('express');
-const app = express();
-const port = 8080;
+const express = require('express')
+const app = express()
+const port = process.env.PORT
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-const {MongoClient} = require('mongodb') 
-const uri = process.env.MONGO_URL;
+const { MongoClient } = require('mongodb') 
+const uri = process.env.MONGO_URL
 const client = new MongoClient(uri)
+
+const fs = require('fs')
+const uploadDir = 'public/uploads/'
+
+const multer = require('multer');
+const path = require('path');
+
+// methodOverride
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+
+// 업로드 디렉션이 없을 경우 만들어 낸다
+if(!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir, {recursive: true});
 
 const getDB = async () => {
   await client.connect()
   return client.db('blog')
 }
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  try{
+    const db = await getDB();
+    const posts = await db.collection('posts').find().toArray();
+    res.render('index', {posts}) // 하나일 때 posts, 여러 개일 때 {posts: posts, ...}
+  }catch(e){
+    console.error(e);
+  }
 })
 
 app.get('/write', (req, res) => {
   res.render('write');
 })
-
-const fs = require('fs');
-const uploadDir = 'public/uploads/';
-
-if(!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
-
-const multer = require('multer');
-const path = require('path');
 
 // Multer 설정
 const storage = multer.diskStorage({
@@ -49,7 +60,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 app.post('/write', upload.single('postImg'), async (req, res) => {
-  const { title, content, createAtDate } = req.body;
+  const { title, content, date } = req.body;
   const postImg = req.file ? req.file.filename : null;
 
   try {
@@ -59,8 +70,8 @@ app.post('/write', upload.single('postImg'), async (req, res) => {
           _id: result.totalPost + 1,
           title,
           content,
-          createAtDate,
-          postImgPath: postImg ? `uploads/${postImg}` : null,
+          date,
+          postImgPath: postImg ? `/uploads/${postImg}` : null,
       });
       await db.collection('counter').updateOne({ name: 'counter' }, { $inc: { totalPost: 1 } });
       res.redirect('/');
@@ -69,7 +80,67 @@ app.post('/write', upload.single('postImg'), async (req, res) => {
   }
 });
 
+// detail
+app.get('/detail/:id', async (req, res) => {
+  console.log(req.params.id);
+  let id = parseInt(req.params.id);
+  try{
+    const db = await getDB();
+    const posts = await db.collection('posts').findOne({_id: id});
+    console.log(posts);
+    res.render('detail', {posts}) // 하나일 때 posts, 여러 개일 때 {posts: posts, ...}
+  }catch(e){
+    console.error(e);
+  }
+})
+
+// delete
+app.post('/delete/:id', async (req, res) => {
+  let id = parseInt(req.params.id)
+  try{
+    const db = await getDB()
+    await db.collection('posts').deleteOne({_id: id})
+    res.redirect('/')
+  }catch(e){
+    console.error(e);
+  }
+})
+
+// edit 페이지로 데이터 바인딩
+app.get('/edit/:id', async (req, res) => {
+  let id = parseInt(req.params.id)
+  try{
+    const db = await getDB()
+    const posts = await db.collection('posts').findOne({_id: id})
+    res.render('edit', {posts}) 
+  }catch(e){
+    console.error(e)
+  }
+})
+
+// /edit post 요청 왔을 때
+app.post('/edit', upload.single('postImg'), async (req, res) => {
+  console.log("----------", req.body)
+  const {id, title, content, date} = req.body
+  const postImgOld = req.body.postImgOld.replace('uploads/', '')
+  const postImg = req.file ? req.file.filename : postImgOld
+
+  try{
+    const db = await getDB()
+    await db.collection('posts').updateOne({_id: parseInt(id)}, {
+      $set: {
+        title, 
+        content, 
+        date, 
+        postImgPath: postImg ? `/uploads/${postImg}` : null,
+      }
+    })
+    res.redirect('/')
+  }catch(e){
+    console.error(e)
+  }
+})
 
 app.listen(port, () => {
-  console.log(` ====== ${port}`);
+  console.log(`====== ${port}`);
 });
--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 require('dotenv').config()
 
-const express = require('express');
-const app = express();
-const port = process.env.PORT;
+const express = require('express')
+const app = express()
+const port = process.env.PORT
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-const {MongoClient} = require('mongodb') 
-const uri = process.env.MONGO_URL;
+const { MongoClient } = require('mongodb') 
+const uri = process.env.MONGO_URL
 const client = new MongoClient(uri)
 
-const fs = require('fs');
-const uploadDir = 'public/uploads/';
+const fs = require('fs')
+const uploadDir = 'public/uploads/'
 
 const multer = require('multer');
 const path = require('path');
 
+// methodOverride
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+
+// 업로드 디렉션이 없을 경우 만들어 낸다
 if(!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir, {recursive: true});
 
 const getDB = async () => {
@@ -31,16 +36,10 @@ app.get('/', async (req, res) => {
   try{
     const db = await getDB();
     const posts = await db.collection('posts').find().toArray();
-    console.log(posts);
-
-    res.render('index', {posts}) // 하나일 때 posts, 여러 개일 때 {posts, ...}
+    res.render('index', {posts}) // 하나일 때 posts, 여러 개일 때 {posts: posts, ...}
   }catch(e){
     console.error(e);
   }
-})
-
-app.get('/', (req, res) => {
-  res.render('index');
 })
 
 app.get('/write', (req, res) => {
@@ -72,7 +71,7 @@ app.post('/write', upload.single('postImg'), async (req, res) => {
           title,
           content,
           date,
-          postImgPath: postImg ? `uploads/${postImg}` : null,
+          postImgPath: postImg ? `/uploads/${postImg}` : null,
       });
       await db.collection('counter').updateOne({ name: 'counter' }, { $inc: { totalPost: 1 } });
       res.redirect('/');
@@ -81,40 +80,123 @@ app.post('/write', upload.single('postImg'), async (req, res) => {
   }
 });
 
+// detail
 app.get('/detail/:id', async (req, res) => {
-  const id = parseInt(req.params.id)
+  console.log(req.params.id);
+  let id = parseInt(req.params.id);
   try{
     const db = await getDB();
-    const post = await db.collection('posts').findOne({_id: id})
-    res.render('detail', {post});
-  }catch(error){
-    console.error(error)
-  }
-});
-
-app.get('/edit/:id', async (req, res) => {
-  const id = parseInt(req.params.id)
-  try{
-    const db = await getDB()
-    const post = await db.collection('posts').findOne({_id: id})
-    res.render('edit', {post})
-  }catch(error){
-    console.error(error)
+    const posts = await db.collection('posts').findOne({_id: id});
+    console.log(posts);
+    res.render('detail', {posts}) // 하나일 때 posts, 여러 개일 때 {posts: posts, ...}
+  }catch(e){
+    console.error(e);
   }
 })
 
-app.post('/delete', async (req, res) => {
-  const id = parseInt(req.body.postNum)
-  console.log(id)
+// delete
+app.post('/delete/:id', async (req, res) => {
+  let id = parseInt(req.params.id)
   try{
     const db = await getDB()
     await db.collection('posts').deleteOne({_id: id})
     res.redirect('/')
-  }catch(error){
-    console.error(error)
+  }catch(e){
+    console.error(e);
+  }
+})
+
+// edit 페이지로 데이터 바인딩
+app.get('/edit/:id', async (req, res) => {
+  let id = parseInt(req.params.id)
+  try{
+    const db = await getDB()
+    const posts = await db.collection('posts').findOne({_id: id})
+    res.render('edit', {posts}) 
+  }catch(e){
+    console.error(e)
+  }
+})
+
+// /edit post 요청 왔을 때
+app.post('/edit', upload.single('postImg'), async (req, res) => {
+  console.log("----------", req.body)
+  const {id, title, content, date} = req.body
+  const postImgOld = req.body.postImgOld.replace('uploads/', '')
+  const postImg = req.file ? req.file.filename : postImgOld
+
+  try{
+    const db = await getDB()
+    await db.collection('posts').updateOne({_id: parseInt(id)}, {
+      $set: {
+        title, 
+        content, 
+        date, 
+        postImgPath: postImg ? `/uploads/${postImg}` : null,
+      }
+    })
+    res.redirect('/')
+  }catch(e){
+    console.error(e)
+  }
+})
+
+// npm에서 bcrypt 참고하여 설치
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+app.get('/signup', (req, res) => {
+  res.render('signup')
+})
+
+app.post('/signup', async (req, res) => {
+  const { userId, pw, userName } = req.body
+  console.log('가입 정보 확인', req.body);
+
+  try{
+    const hashedPw = await bcrypt.hash(pw, saltRounds); // 비밀번호 암호화
+    const db = await getDB();
+    await db.collection('users').insertOne({
+      userId,
+      pw: hashedPw,
+      userName
+    })
+    res.redirect('/login')
+  }catch(e){
+    console.error(e);
+  }
+})
+
+// login
+app.get('/login', async (req, res) => {
+  res.render('login')
+})
+
+app.post('/login', async (req, res) => {
+  const {userId, pw} = req.body;
+  
+  try{
+    const db = await getDB();
+    const user = await db.collection('users').findOne({userId});
+    console.log('로그인 데이터', req.body, user);
+
+    if(user){
+      const compareResult = await bcrypt.compare(pw, user.pw);
+      if(compareResult){
+        res.redirect('/')
+      } else {
+        // 비밀번호가 맞지 않을 경우
+        res.status(401).send()
+      }
+    } else {
+      // 아이디가 없을 때
+      res.status(404).send()
+    }
+  }catch(e){
+    console.error(e);
   }
 })
 
 app.listen(port, () => {
-  console.log(` ====== ${port}`);
+  console.log(`====== ${port}`);
 });
