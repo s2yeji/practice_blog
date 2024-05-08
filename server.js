2521e37cccab6fb896b9357993286a8c4153c71e
s2yeji <s2seolyeji@gmail.com>
2024-05-08 16:11:12 +0900

    2024-05-08 edit

--- server.js
+++ server.js
@@ -24,6 +24,43 @@ const path = require('path');
 const methodOverride = require('method-override')
 app.use(methodOverride('_method'))
 
+// jsonwebtoken
+// 로그인 성공 시 토큰 발행(어떤 정보를 넣어서 발행할지 결정할 수 있음)
+// 쿠키에 토큰 저장
+// 토큰 정보를 header.ejs에 반영
+// 로그아웃 시 쿠키 삭제
+// 로그인 했을 때 ui를 다르게 할 수 있음
+// 토큰 만료 시간 설정 가능
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET_KEY;
+
+// cookie-parser
+// 쿠키 파싱
+// 쿠키 생성 및 삭제
+// 쿠키를 데이터에 저장
+const cookieParser = require('cookie-parser');
+app.use(cookieParser()) // cookieParser에 대한 미들웨어 혹은 라이브러리 내지는 서드파티
+
+// 모든 요청에 대해 쿠키를 검사
+// 토큰이 있다면 토큰을 해독해서 user 정도만 추출
+// req.user 영역에 userId를 저장
+app.use(async (req, res, next) => {
+  const token = req.cookies.token;
+  
+  if(token){ // true인 경우 로그인 됨을 의미
+    try{
+      const data = jwt.verify(token, SECRET);
+      const db = await getDB();
+      const user = await db.collection('users').findOne({userId: data.userId});
+      req.user = user ? user : null;
+    }catch(e){
+      console.error(e);
+    }
+  }
+  next();
+})
+
+
 // 업로드 디렉션이 없을 경우 만들어 낸다
 if(!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir, {recursive: true});
 
@@ -33,17 +70,19 @@ const getDB = async () => {
 }
 
 app.get('/', async (req, res) => {
+  // const user = req.user ? req.user : null;
+  console.log("--------------", req.user);
   try{
     const db = await getDB();
     const posts = await db.collection('posts').find().toArray();
-    res.render('index', {posts}) // 하나일 때 posts, 여러 개일 때 {posts: posts, ...}
+    res.render('index', {posts, user: req.user}) // 하나일 때 posts, 여러 개일 때 {posts: posts, ...}
   }catch(e){
     console.error(e);
   }
 })
 
 app.get('/write', (req, res) => {
-  res.render('write');
+  res.render('write', {user: req.user});
 })
 
 // Multer 설정
@@ -60,7 +99,8 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 app.post('/write', upload.single('postImg'), async (req, res) => {
-  const { title, content, date } = req.body;
+  // const user = req.user ? req.user : null;
+  const { title, content, date, userId } = req.body;
   const postImg = req.file ? req.file.filename : null;
 
   try {
@@ -70,10 +110,14 @@ app.post('/write', upload.single('postImg'), async (req, res) => {
           _id: result.totalPost + 1,
           title,
           content,
+          userId: req.user.userId,
+          userName: req.user.userName,
           date,
           postImgPath: postImg ? `/uploads/${postImg}` : null,
       });
       await db.collection('counter').updateOne({ name: 'counter' }, { $inc: { totalPost: 1 } });
+      // 좋아요 기본값 세팅 
+      await db.collection('like').insertOne({post_id: result.totalPost + 1, totalLike: 0, likeMember: []})
       res.redirect('/');
   } catch (error) {
       console.log(error);
@@ -87,8 +131,8 @@ app.get('/detail/:id', async (req, res) => {
   try{
     const db = await getDB();
     const posts = await db.collection('posts').findOne({_id: id});
-    console.log(posts);
-    res.render('detail', {posts}) // 하나일 때 posts, 여러 개일 때 {posts: posts, ...}
+    const like = await db.collection('like').findOne({post_id: id});
+    res.render('detail', {posts, user: req.user, like}) // 하나일 때 posts, 여러 개일 때 {posts: posts, ...}
   }catch(e){
     console.error(e);
   }
@@ -108,11 +152,12 @@ app.post('/delete/:id', async (req, res) => {
 
 // edit 페이지로 데이터 바인딩
 app.get('/edit/:id', async (req, res) => {
+  const user = req.user ? req.user : null;
   let id = parseInt(req.params.id)
   try{
     const db = await getDB()
     const posts = await db.collection('posts').findOne({_id: id})
-    res.render('edit', {posts}) 
+    res.render('edit', {posts, user: req.user}) 
   }catch(e){
     console.error(e)
   }
@@ -146,7 +191,7 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 app.get('/signup', (req, res) => {
-  res.render('signup')
+  res.render('signup', {user: req.user})
 })
 
 app.post('/signup', async (req, res) => {
@@ -169,20 +214,22 @@ app.post('/signup', async (req, res) => {
 
 // login
 app.get('/login', async (req, res) => {
-  res.render('login')
+  res.render('login', {user: req.user})
 })
 
 app.post('/login', async (req, res) => {
-  const {userId, pw} = req.body;
+  const {userId, pw} = req.body; // 인풋 요소에서 받아온 것
   
   try{
     const db = await getDB();
     const user = await db.collection('users').findOne({userId});
-    console.log('로그인 데이터', req.body, user);
+    console.log('로그인 데이터', user);
 
     if(user){
       const compareResult = await bcrypt.compare(pw, user.pw);
       if(compareResult){
+        const token = jwt.sign({userId: user.userId}, SECRET) // 토큰 발행
+        res.cookie('token', token)
         res.redirect('/')
       } else {
         // 비밀번호가 맞지 않을 경우
@@ -197,6 +244,62 @@ app.post('/login', async (req, res) => {
   }
 })
 
+app.get('/logout', (req, res) => {
+  res.clearCookie('token');
+  res.redirect('/');
+})
+
+// personal
+app.get('/personal/:userId', async (req, res) => {
+  const postUser = req.params.userId;
+  try{
+    const db = await getDB();
+    const user = await db.collection('users').findOne({ userId: postUser });
+    const posts = await db.collection('posts').find({userId: postUser}).toArray();
+    console.log(postUser, posts);
+    res.render('personal', {
+      posts, 
+      user: req.user, 
+      userName: user.userName, 
+      postUser
+    }) // posts[0].userName = 닉네임, postUser = 아이디 노출
+  }catch(e){
+    console.error(e);
+  }
+})
+
+// mypage
+app.get('/mypage', (req, res) => {
+  console.log(req.user);
+  res.render('mypage', {user: req.user});
+})
+
+// like
+// /like/2 post 요청이 들어오면 로그인된 userId
+app.post('/like/:id', async (req, res)=>{
+  const postId = parseInt(req.params.id); // 몇 번째 포스트를 클릭했는지 즉 포스트 아이디
+  const userId = req.user.userId; // 로그인된 사용자
+  try{
+    const db = await getDB();
+    const like = await db.collection('like').findOne({post_id: postId});
+    if(like.likeMember.includes(userId)){ // 이미 좋아요를 누른 경우
+      await db.collection('like').updateOne({post_id: postId}, {
+        $inc: {totalLike: -1},
+        $pull: {likeMember: userId}
+      })
+    } else { // 좋아요를 처음 누르는 경우
+      await db.collection('like').updateOne({post_id: postId}, {
+        $inc: {totalLike: 1},
+        $push: {likeMember: userId}
+      })
+    }
+    res.redirect('/detail/' + postId)
+  }catch(e){
+    console.error(e)
+  }
+})
+
+
 app.listen(port, () => {
-  console.log(`====== ${port}`);
+  console.log(`서버 실행 중 ${port}`);
 });
